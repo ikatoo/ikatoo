ad1d835c895e8eda2c556f81b74f3ecf169cb4e2
Milton Carlos Katoo <mckatoo@gmail.com>
2022-12-30 19:02:13 -0300

    implicited env vars

--- backend/auth/src/keycloak.ts
+++ backend/auth/src/keycloak.ts
@@ -1,13 +1,13 @@
 import KeycloakConnect, { KeycloakConfig } from 'keycloak-connect'
-import { env } from './env'
+import env from './env'
 import session from 'express-session'
 
 export const memoryStore = new session.MemoryStore()
 
 const config: KeycloakConfig = {
-  realm: env('KC_REALM'),
-  resource: env('KC_CLIENT_ID'),
-  'auth-server-url': env('KC_AUTH_SERVER_URL'),
+  realm: env.KC_REALM,
+  resource: env.KC_CLIENT_ID,
+  'auth-server-url': env.KC_AUTH_SERVER_URL,
   'confidential-port': 0,
   'ssl-required': 'external'
 }
